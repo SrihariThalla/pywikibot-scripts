4a9387e7bb637ba002e7086dcb96bd9da75aed37
Srihari Thalla <thallasrihari@gmail.com>
2020-12-31 15:41:39 +0100

    Update add coordinates to railway stations in India

--- ir_add_coord.py
+++ ir_add_coord.py
@@ -1,3 +1,9 @@
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+#
+
 import pywikibot
 import os
 import overpy
@@ -6,6 +12,53 @@ from pywikibot import pagegenerators as pg
 
 def print_log(item: pywikibot.ItemPage, message: str):
     print('%s - %s - %s' % (message, item.labels.get('en', 'No EN label'), item.getID()))
+
+
+def get_location(item_id):
+    # result = api.query("""
+    #         node["wikidata"="{0}"];
+    #         (._;>;);
+    #         out body;
+    #         """.format(item_id))
+    #
+    # if len(result.nodes) == 1:
+    #     return result.nodes[0]
+    #
+    # if len(result.nodes) > 1:
+    #     print_log(item, 'Too many nodes found for')
+    #     print()
+    #
+    #     return False
+
+    result = api.query("""
+                way["wikidata"="{0}"];
+                (._;>;);
+                out body;
+                """.format(item_id))
+
+    # if len(result.ways) == 1:
+    #     return result.ways[0]
+
+    if len(result.ways) > 1:
+        print_log(item, 'Too many ways found for')
+        print()
+
+        return False
+
+    print(result.ways)
+
+    result = api.query("""
+                    relation["wikidata"="{0}"];
+                    (._;>;);
+                    out body;
+                    """.format(item_id))
+
+    print(result.relations)
+
+    if len(result.relations) == 1:
+        return result.relations
+
+    return False
 
 
 api = overpy.Overpass()
@@ -19,6 +72,15 @@ generator = pg.WikidataSPARQLPageGenerator(query, site)
 
 for item in generator:
     item.get()
+
+    # location = get_location(item.getID())
+    #
+    # print(location)
+    #
+    # if not location:
+    #     print_log(item, 'No location found for')
+    #
+    # continue
 
     result = api.query("""
         node["wikidata"="{0}"];
